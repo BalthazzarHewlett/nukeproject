90f035a8ee48d00e8629554d3aeb36a834521122
BalthazzarHewlett <82304278+BalthazzarHewlett@users.noreply.github.com>
2021-04-11 14:49:21 +0530

    Add files via upload
    installer bashscript

---
+++ install.sh
@@ -0,0 +1,3 @@
+echo "better to install and troubleshoot yourself"
+pip install colored
+apt install nmap ldb dnsenum dnsrecon fierce nikto skipfish whatweb joomscan wpscan framework slowhttptest dirb gobuster seclists
